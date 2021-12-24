ee961e5e25950c21cbfafabde7debde8ebff4357
Shafqat Rasool <53371063+Shafqatrasool13000@users.noreply.github.com>
2021-12-24 05:31:22 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import TodoInput from './components/TodoInput';
-import DynamicInput from './components/DynamicInput';
+
 
 function App() {
   return (
     <div className="App">
       <TodoInput/>
-      {/* <DynamicInput/> */}
     </div>
   );
 }
